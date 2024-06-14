f8e5b891ac38cbdbdbf55be203bbf658d21713b0
Vlad Utkin <vlad.utkin@yahoo.com>
2024-06-14 17:46:00 +0200

    CI was modified

--- playwright.config.ts
+++ playwright.config.ts
@@ -94,7 +94,7 @@ export default defineConfig({
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  outputDir: 'e2e/report',
 
   /* Run your local dev server before starting the tests */
   webServer: {
